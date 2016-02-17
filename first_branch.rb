4471684ce3c17ae4d353828fe050daca6ea2e274
Gavyn Caldwell <gavyncaldwell@gmail.com>
2016-02-17 11:33:47 -0700

    added first_branch.rb

---
+++ first_branch.rb
@@ -0,0 +1,10 @@
+puts "first Branch"
+puts "what is your favorit color"
+favorite_color = gets.strip
+  if favorite_color == "black"
+    puts "you depressed bro"
+  else
+    puts "nice"
+#still need to grab user input
+#still need to put output more info
+#still need to get more user input
